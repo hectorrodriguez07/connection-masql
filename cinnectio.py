5e628817e604768bdaf879e9428d538cac307ef4
hectorrodriguez07 <josuediscua07@gmail.com>
2021-04-27 11:23:44 -0600

    conexion2

---
+++ cinnectio.py
@@ -0,0 +1,8 @@
+import mysql.connector.connect(host="localhost",user="root",passwd="")
+
+cursor=connection.cursor()
+cursor.execute("show databases")
+ for base in cursor:
+     print(base)
+
+connection.close()
